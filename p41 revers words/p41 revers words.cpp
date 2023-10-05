e39fd0cbe74d62b047b52da1827fb441d7da9b2c
gomaa <gogochemist35@gmail.com>
2023-10-05 17:16:35 +0300

    change code

--- p41 revers words/p41 revers words.cpp
+++ p41 revers words/p41 revers words.cpp
@@ -13,72 +13,109 @@ string readString()
 	return str;
 }
 
-vector <string> formStrWords(string str)
+vector <string> splitString(string str, string delim)
 {
 	vector <string>vStr;
 	string word = "";
 	short pos = 0;
-	string delim = " ";
-	string S1 = "";
-	//"Mohammad Abu-Hadhoud Iam from Jordan"
 	
-	while ((pos = str.find(delim)) != -1)
-	{
-		word = str.substr(0, pos);
-		str = str.erase(0, pos +delim.length());
-		if (word != "")
-			//vStr.push_back(word);
-			S1 = word + S1;
-	}
-	if (str != "")
-		//vStr.push_back(str);
-		S1 = str + S1;
-
-	return vStr;
-}
-
-void reversStr(string str)
-{
-	string word = "";
-	short pos = 0;
-	string delim = " ";
-	string S1 = "";
 	//"Mohammad Abu-Hadhoud Iam from Jordan"
-
 	while ((pos = str.find(delim)) != -1)
 	{
 		word = str.substr(0, pos);
 		str = str.erase(0, pos + delim.length());
 		if (word != "")
-			S1 = word +delim+ S1;
+			vStr.push_back(word);
 	}
 	if (str != "")
-		S1 = str +delim + S1;
+		vStr.push_back(str);
 
-	cout << S1 << endl;
+	return vStr;
 }
-void printReverseStr(vector <string>vStr)
+
+void reversStr(vector <string>vStr)
 {
-	for (int i = vStr.size() - 1; i >= 0; i--)
+	string reversedString = "";
+	string delim = " ";
+
+	vector<string>::iterator iter = vStr.end();
+
+	while (iter != vStr.begin())
 	{
-		cout << vStr[i] << " ";
+		iter--;
+		reversedString += *iter + " ";
 	}
+	reversedString = reversedString.substr(0, reversedString.length()-1);
+
+	cout << reversedString << endl<< endl;
 }
 
 
 
 int main()
 {
-	//string str = readString();
 	//string str = "Mohammad Abu-Hadhoud Iam from Jordan";
-	string str = "Gomaa ElSherbini Mohammad";
+	string str = readString();
+	vector <string> vStr = splitString(str, " ");
 
-	cout << "string after reversing: \n";
+	cout << "\n\nstring after reversing: \n";
+	reversStr(vStr);
 
 	//reverseStr(formStrWords(str));
-	reversStr(str);
+	//reversStr(str);
 	cout << endl<< endl;
 
 
 	return 0;
 }
+
+//vector <string> formStrWords(string str)
+//{
+//	vector <string>vStr;
+//	string word = "";
+//	short pos = 0;
+//	string delim = " ";
+//	string S1 = "";
+//	//"Mohammad Abu-Hadhoud Iam from Jordan"
+//	
+//	while ((pos = str.find(delim)) != -1)
+//	{
+//		word = str.substr(0, pos);
+//		str = str.erase(0, pos +delim.length());
+//		if (word != "")
+//			//vStr.push_back(word);
+//			S1 = word + S1;
+//	}
+//	if (str != "")
+//		//vStr.push_back(str);
+//		S1 = str + S1;
+//
+//	return vStr;
+//}
+//void reversStr(string str)
+//{
+//	string word = "";
+//	short pos = 0;
+//	string delim = " ";
+//	string S1 = "";
+//	//"Mohammad Abu-Hadhoud Iam from Jordan"
+//
+//	while ((pos = str.find(delim)) != -1)
+//	{
+//		word = str.substr(0, pos);
+//		str = str.erase(0, pos + delim.length());
+//		if (word != "")
+//			S1 = word +delim+ S1;
+//	}
+//	if (str != "")
+//		S1 = str +delim + S1;
+//
+//	cout << S1 << endl;
+//}
+//void printReverseStr(vector <string>vStr)
+//{
+//	for (int i = vStr.size() - 1; i >= 0; i--)
+//	{
+//		cout << vStr[i] << " ";
+//	}
+//}
